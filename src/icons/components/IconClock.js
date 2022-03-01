0b015db1a7fcb48685f1d90900547eae727801f9
Albin Frias <albinesteban@outlook.com>
2022-03-01 15:07:07 -0400

    v0.3.3
    * feat: add iconclock, iconeyeoff, iconeyeon and added textcopywithpassword component
    
    * v0.3.2
    
    * Fix: added correct types to icons
    
    * chore: allow banner to have icon on mobile version
    
    * v0.3.3

---
+++ src/icons/components/IconClock.js
@@ -0,0 +1,36 @@
+import React from "react";
+import useIconSize from "../icon-size";
+import IconPropTypes from "../IconPropTypes";
+
+function IconClock({ size, ...props }) {
+  const sizeValue = useIconSize(size);
+
+  return (
+    <svg
+      width={sizeValue}
+      height={sizeValue}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12 6V12L16 14"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+IconClock.propTypes = IconPropTypes;
+export default IconClock;
