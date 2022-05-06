5900c83fc646dea9a141b9874302e4752517ebc8
Albin <albinesteban@outlook.com>
2022-05-06 14:21:57 -0400

    feat: update banner mobile styles

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,15 +8,23 @@ import { IconInfo, IconThickCross } from '../../icons'
 
 function useModeColor(mode) {
   const theme = useTheme()
+  const { within } = useViewport()
+  const compactMode = within(-1, 'medium')
 
   if (mode === 'error') {
-    return `linear-gradient(270deg, #1B2331 36.46%, #3F0B0B 100%)`
+    return compactMode
+      ? `linear-gradient(106.2deg, #3A2020 0%, #262A34 100%)`
+      : `linear-gradient(270deg, #1B2331 36.46%, #3F0B0B 100%)`
   }
   if (mode === 'warning') {
-    return `linear-gradient(270deg, ${theme.surface} 36.46%, #816110 100%)`
+    return compactMode
+      ? `linear-gradient(106.2deg, #2E2818 0%, #262A34 100%)`
+      : `linear-gradient(270deg, ${theme.surface} 36.46%, #816110 100%)`
   }
   if (mode === 'info') {
-    return `linear-gradient(270deg, ${theme.surface} 36.46%, #104A64 100%)`
+    return compactMode
+      ? `linear-gradient(106.2deg, #243A4F 0%, #262A34 100%)`
+      : `linear-gradient(270deg, ${theme.surface} 36.46%, #104A64 100%)`
   }
 
   return `background: ${theme.surface}`
@@ -65,33 +73,35 @@ function Banner({ children, mode = 'info', title }) {
         background: ${modeBackground};
         display: grid;
         grid-template-columns: ${compactMode ? '1fr' : `${12 * GU}px 1fr`};
-        padding: ${3 * GU}px ${4 * GU}px;
+        padding: ${compactMode ? `${GU * 2}px` : `${3 * GU}px ${4 * GU}px`};
         border-radius: ${RADIUS}px;
       `}
     >
-      <div
-        css={`
-          width: ${12 * GU}px;
-          height: 100%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        `}
-      >
+      {!compactMode && (
         <div
           css={`
-            background: ${titleColor};
-            width: ${4 * GU}px;
-            height: ${4 * GU}px;
-            border-radius: 50% 50%;
+            width: ${12 * GU}px;
+            height: 100%;
             display: flex;
             justify-content: center;
             align-items: center;
           `}
         >
-          {modeIcon}
+          <div
+            css={`
+              background: ${titleColor};
+              width: ${4 * GU}px;
+              height: ${4 * GU}px;
+              border-radius: 50% 50%;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            `}
+          >
+            {modeIcon}
+          </div>
         </div>
-      </div>
+      )}
       <div
         css={`
           width: 100%;
@@ -105,6 +115,8 @@ function Banner({ children, mode = 'info', title }) {
             ${textStyle('title3')}
             font-weight: bold;
             color: ${titleColor};
+            margin-bottom: ${compactMode ? '0' : '16'}px;
+            margin-top: ${compactMode ? '0' : '16'}px;
           `}
         >
           {title}
@@ -112,7 +124,10 @@ function Banner({ children, mode = 'info', title }) {
         <Spacer size={2 * GU} />
         <p
           css={`
-            ${textStyle('body3')}
+            ${textStyle('body3')};
+            margin-bottom: ${compactMode ? '0' : '16'}px;
+            margin-top: ${compactMode ? '0' : '16'}px;
+            color: ${theme.content};
           `}
         >
           {children}
